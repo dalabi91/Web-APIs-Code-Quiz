3b8bd746de4db010bc13ecd0f8080524bf329a1b
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-01-13 22:41:12 +0000

    added quiz questions and links js file to the html file

---
+++ questions.js
@@ -0,0 +1,58 @@
+//quiz question and answers
+var question =  [
+ {
+    question: "What does 'JS' stand for?",
+    choices: ["Java Source", "JavaScript", "JumboScript", "JustScript"],
+    correctAnswer: "JavaScript"
+  },
+  {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    choices: ["var", "let", "const", "both var and let"],
+    correctAnswer: "both var and let"
+  },
+  {
+    question: "What is the result of the expression 2 + '2' in JavaScript?",
+    choices: ["4", "22", "Error", "NaN"],
+    correctAnswer: "22"
+  },
+  {
+    question: "Which function is used to print output in JavaScript?",
+    choices: ["console.log()", "print()", "output()", "log()"],
+    correctAnswer: "console.log()"
+  },
+  {
+  question: "Which of the following is a JavaScript framework?",
+  choices: ["Angular", "React", "Vue", "All of the above"],
+  correctAnswer: "All of the above"
+},
+{
+  question: "What is the purpose of the 'document.getElementById()' method?",
+  choices: [
+    "To select an element by its class",
+    "To select an element by its tag name",
+    "To select an element by its ID",
+    "To select an element by its name"
+  ],
+  correctAnswer: "To select an element by its ID"
+},
+{
+  question: "What is the correct way to comment out multiple lines in JavaScript?",
+  choices: [
+    "// This is a comment",
+    "/* This is a comment */",
+    "# This is a comment",
+    "' This is a comment"
+  ],
+  correctAnswer: "/* This is a comment */"
+},
+{
+  question: "What is the purpose of the 'Array.isArray()' method?",
+  choices: [
+    "To check if a variable is an array",
+    "To reverse the elements of an array",
+    "To remove an element from an array",
+    "To add an element to an array"
+  ],
+  correctAnswer: "To check if a variable is an array"
+}
+];
